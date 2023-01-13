7652f7a43ad700ad051d083fa27fab2288d5dbfb
eddyK15501 <edk1231@gmail.com>
2023-01-12 21:26:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Blog from "./pages/Blog/Blog";
+import Resume from './pages/Resume';
+import BlogOne from './pages/Blog/BlogOne';
+import BlogTwo from './pages/Blog/BlogTwo';
+import BlogThree from './pages/Blog/BlogThree';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <Router>
+
+        <div className="pages">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/resume" element={<Resume />} />
+            
+            <Route path="/blog/blogone" element={<BlogOne />} />
+            <Route path="/blog/blogtwo" element={<BlogTwo />} />
+            <Route path="/blog/blogthree" element={<BlogThree />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
